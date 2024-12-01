5a7231757dce914175a5eda838c9b7213741a056
Александр Мехаев <alexandmekhaev@yandex.ru>
2024-12-01 19:36:01 +0300

    init

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,9 +2,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
+router.register('api-token-auth', CatViewSet)
+router.register('posts', CatViewSet)
+router.register('groups', CatViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
 ]
 
 
